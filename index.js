b4eae8ab40eeccb3dea82f2cc8b8e016e14f204b
dynamic-chain-5255 <ritiguda@gmail.com>
2021-01-13 18:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function multiply(a,b){
 function divide(a,b){
   return a / b
 }
+function increment(n){
+  return n += 1
+}
+function decrement(n){
+  return n -= 1
+}
+function makeInt(string){
+  return parseInt(string, 10)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
+}
